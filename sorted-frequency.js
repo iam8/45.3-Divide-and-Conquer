2ade4a544208b69d57bb2ffe80e19523e664a7c1
alexm <alex-m@outlook.com>
2023-08-21 15:54:03 -0700

    Add docstring to function and handle edge case for empty array

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,5 +1,18 @@
-function sortedFrequency() {
 
+/**
+ * Return the count of the occurrences of a given number in a given array.
+ *
+ * Return -1 if the given number doesn't exist in the array.
+ *
+ * CONSTRAINT: Given array must be sorted (ascending order).
+ *
+ * Time complexity: O(log n)
+ */
+function sortedFrequency(arr) {
+    const len = arr.length;
+
+    if (len === 0) return 0;
 }
 
-module.exports = sortedFrequency
+
+module.exports = sortedFrequency;
