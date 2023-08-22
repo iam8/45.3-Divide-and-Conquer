4e624ed853c97cfad8a9d283493c946a8a7e07f2
alexm <alex-m@outlook.com>
2023-08-21 21:50:54 -0700

    Add more assertions to test

--- find-rotated-index.test.js
+++ find-rotated-index.test.js
@@ -6,8 +6,11 @@ describe("#findRotatedIndex", function(){
         expect(findRotatedIndex([3, 4, 1, 2], 4)).toBe(1)
         expect(findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 8)).toBe(2)
         expect(findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 3)).toBe(6)
+        expect(findRotatedIndex([37, 44, 66, 102, 10, 22], 10)).toBe(4)
         expect(findRotatedIndex([37, 44, 66, 102, 10, 22], 14)).toBe(-1)
         expect(findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12)).toBe(-1)
+        expect(findRotatedIndex([9], 9)).toBe(0);
+        expect(findRotatedIndex([9], 1)).toBe(-1);
         expect(findRotatedIndex([], 9)).toBe(-1);
     })
 })
