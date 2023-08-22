7023765dd5c3df8310242ea234a12fd172ea01ac
alexm <alex-m@outlook.com>
2023-08-21 18:44:49 -0700

    Add docstring and params to function

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,16 @@
-function findRotatedIndex() {
- 
+
+/**
+ * Return the index of a target value in a rotated array.
+ *
+ * @param {Number[]} rotArr A rotated array of sorted numbers
+ * @param {Number} target Value to search for in array
+ * @returns The index of the target value, or -1 if not found
+ */
+function findRotatedIndex(rotArr, target) {
+    const len = rotArr.length;
+
+
 }
 
-module.exports = findRotatedIndex
+
+module.exports = findRotatedIndex;
