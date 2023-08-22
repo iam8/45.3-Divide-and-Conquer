6f2bf7e16c49aaae501013fc701b07b0a9c9cd1f
alexm <alex-m@outlook.com>
2023-08-21 22:03:50 -0700

    Add docstring to function and edge case for small or empty array

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,21 @@
-function findRotationCount() {
-  
+
+/**
+ * Find the number of times a given array of sorted, distinct numbers has been rotated
+ * (counter-clockwise).
+ *
+ * @param {*} arr An array of sorted (ascending order), distinct numbers, rotated counter-clock-
+ * wise a number of times
+ * @returns The number of times the given array has been rotated
+ */
+function findRotationCount(arr) {
+    const len = arr.length;
+
+    if (len === 0 || len === 1) return 0;
+
+    let rotations = 0;
+
+    return rotations;
 }
 
-module.exports = findRotationCount
+
+module.exports = findRotationCount;
