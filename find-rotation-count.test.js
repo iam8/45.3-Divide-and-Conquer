e998e2205c550f4bdc371622fe85391ce7efee0a
alexm <alex-m@outlook.com>
2023-08-21 22:03:26 -0700

    Add assertions to test

--- find-rotation-count.test.js
+++ find-rotation-count.test.js
@@ -6,5 +6,7 @@ describe("#findRotationCount", function () {
         expect(findRotationCount([15, 18, 2, 3, 6, 12])).toBe(2)
         expect(findRotationCount([7, 9, 11, 12, 5])).toBe(4)
         expect(findRotationCount([7, 9, 11, 12, 15])).toBe(0)
+        expect(findRotationCount([1])).toBe(0);
+        expect(findRotationCount([])).toBe(0);
     })
 })
