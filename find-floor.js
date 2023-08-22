61e433662ebed730718f2b50afef5ec7de319a53
alexm <alex-m@outlook.com>
2023-08-21 22:43:03 -0700

    Add params and docstring to function

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,19 @@
-function findFloor() {
-  
+
+/**
+ * Return the floor of the number x in the given array.
+ *
+ * The floor of x in an array is defined as the largest element in the array which is smaller than
+ * or equal to x.
+ *
+ * Time complexity: O(log n)
+ *
+ * @param {Number[]} arr A sorted array of numbers (ascending order)
+ * @param {Number} x A value to find the floor of in the array
+ * @returns The floor of x, or -1 if a floor doesn't exist in the given array
+ */
+function findFloor(arr, x) {
+
 }
 
-module.exports = findFloor
+
+module.exports = findFloor;
