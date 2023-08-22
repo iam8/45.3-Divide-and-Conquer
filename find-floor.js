888ed6ac4d682aea60372454f69ae7f6955ea9a7
alexm <alex-m@outlook.com>
2023-08-21 23:19:37 -0700

    Add working logic to function

--- find-floor.js
+++ find-floor.js
@@ -12,7 +12,40 @@
  * @returns The floor of x, or -1 if a floor doesn't exist in the given array
  */
 function findFloor(arr, x) {
+    const len = arr.length;
 
+    if (len === 0) return -1;
+
+    let leftSearchIdx = 0;
+    let rightSearchIdx = len - 1;
+
+    while (leftSearchIdx <= rightSearchIdx) {
+        let midIdx = Math.floor((leftSearchIdx + rightSearchIdx) / 2);
+        let midVal = arr[midIdx];
+
+        if (midVal === x) {
+            return midVal;
+        }
+
+        if (midVal > x) {
+
+            // Search left half
+            rightSearchIdx = midIdx - 1;
+        }
+
+        if (midVal < x) {
+            let rightAdj = arr[midIdx + 1];
+            if (rightAdj === undefined || rightAdj > x) {
+                return midVal;
+            } else {
+
+                // Search right half
+                leftSearchIdx = midIdx + 1;
+            }
+        }
+    }
+
+    return -1;
 }
 
 
