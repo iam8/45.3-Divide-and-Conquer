5daca6175c5ed4e8c071994814613bb792fd24e0
alexm <alex-m@outlook.com>
2023-08-21 22:38:49 -0700

    Add assertions to test

--- find-floor.test.js
+++ find-floor.test.js
@@ -6,6 +6,8 @@ describe("#findFloor", function(){
         expect(findFloor([1, 2, 8, 10, 10, 12, 19], 5)).toBe(2) // 2
         expect(findFloor([1, 2, 8, 10, 10, 12, 19], 20)).toBe(19) // 19
         expect(findFloor([1, 2, 8, 10, 10, 12, 19], 0)).toBe(-1) // -1
-
+        expect(findFloor([1]), 3).toBe(1);
+        expect(findFloor([7]), 1).toBe(-1);
+        expect(findFloor([]), 1).toBe(-1);
     })
 })
